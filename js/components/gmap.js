9a58ca0f569fc6f8f18e8467300d02190901e0a2
grace jiang <gjiang00@gmail.com>
2017-07-17 05:53:30 -0400

    Add files via upload

---
+++ js/components/gmap.js
@@ -0,0 +1,54 @@
+"use strict";
+
+window.initMap = function() {
+  var customMapType = new google.maps.StyledMapType([
+    {
+      stylers: [
+        {'saturation': -100},
+        {'lightness': 50},
+        {'visibility': 'simplified'}
+      ]
+    },
+    {
+      elementType: 'labels',
+      stylers: [{visibility: 'on'}]
+    },
+    {
+      featureType: 'road',
+      stylers: [{color: '#bbb'}]
+    }
+  ], {
+    name: 'Dublin'
+  });
+
+  var image = new google.maps.MarkerImage(
+  	'img/widgets/gmap-pin.png',
+  	new google.maps.Size(48,54),
+  	new google.maps.Point(0,0),
+  	new google.maps.Point(24,54)
+	);
+
+  var customMapTypeId = 'custom_style';
+
+  var fremont = {lat: 37.55, lng: -121.97};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    scrollwheel: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    center: fremont, // Fremont.
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+    }
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    icon: image,
+    title: 'Fremont',
+    position: fremont
+  });
+
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
+}
